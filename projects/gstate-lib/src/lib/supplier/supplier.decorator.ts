8277c4248d4673c93f8d4678c4bc176c600cf766
matt62king <matt62king@gmail.com>
2019-06-02 16:24:46 -0500

    added pull

--- projects/gstate-lib/src/lib/supplier/supplier.decorator.ts
+++ projects/gstate-lib/src/lib/supplier/supplier.decorator.ts
@@ -24,6 +24,26 @@ export const Patch = (metaKey: any): PropertyDecorator => {
   };
 };
 
+export const Pull = (metaKey: any, targetProperty: string | symbol): PropertyDecorator => {
+  return (target: any, key: string | symbol, descriptor?: TypedPropertyDescriptor<Lambda>) => {
+    let originalValue = (): any => {};
+
+    function wrapped(...args: any[]) {
+      console.log(target);
+      originalValue.apply(target, args);
+      const supplier: SupplierRegistryService = InjectorInstance.get<SupplierRegistryService>(SupplierRegistryService);
+      target[targetProperty] = supplier.pull(metaKey);
+    }
+
+    Object.defineProperty(target, key, {
+      set: (lambda: Lambda) => originalValue = lambda,
+      get: () => wrapped
+    });
+
+    return descriptor;
+  };
+};
+
 export const Push = (): PropertyDecorator => {
   return (target: any, key: string | symbol, descriptor?: TypedPropertyDescriptor<Lambda>) => {
     let originalValue = (): any => {};
